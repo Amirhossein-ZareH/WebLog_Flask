3826c3963aedd91b04b816674ff63f6d4ac25fda
Amirhossein-ZareH <zare20212021@gmail.com>
2026-01-12 13:40:32 +0330

    final

--- blog_flask/app.py
+++ blog_flask/app.py
@@ -23,6 +23,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80) , unique = True , nullable=False)
     email = db.Column(db.String(120) , unique =  True , nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
+    #7
+    posts = db.relationship("Post" , backref = "author" , lazy = True)
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -41,6 +43,8 @@ class Post(db.Model):
     title = db.Column(db.String(200) , nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable =False, default=datetime.utcnow)
+    #8
+    user_id = db.Column(db.Integer , db.ForeignKey("user.id") , nullable=False)
 
 # ORM
 @app.route('/')
@@ -101,19 +105,56 @@ def logout():
     logout_user()
     return redirect(url_for("index"))
 
+@app.route('/create_post' , methods=["GET", "POST"])
+@login_required
+def create_post():
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        
+        if not title or content:
+            flash("عنوان و محتوا الزامی میباشد!!!" , "danger")
+            return render_template("create_post.html")
+        
+        post = Post(title=title , content=content , user_id=current_user.id)
+        db.session.add(post)
+        db.session.commit()
+        flash("مقاله شما با موفقیت ذخیره شد")
+        return redirect(url_for("index"))
+    return render_template('create_post.html')
+
+@app.route('/edit_post/<int : id>' , methods=["GET", "POST"])
+@login_required
+def create_post(id):
+    post = Post.query.get_or_404(id)
+    if post.author != current_user:
+        flash("شما اجازه ویرایش این پست را ندارید" , "danger")
+        return render_template("edit_post.html")
+    
+    if request.method == "POST":
+        post.title = request.form["title"]
+        post.content = request.form["content"]
+        
+        if not post.title or post.content:
+            flash("عنوان و محتوا الزامی میباشد!!!" , "danger")
+            return render_template("edit_post.html")
+        
+        db.session.commit()
+        
 def create_tables():
     with app.app_context():
         db.create_all()
         
-        if Post.query.count() == 0:
-            sample_post = [
-                Post(title="مقاله لیزر", content="متن مقاله لیزر"),
-                Post(title="مقاله هوش مصنوعی", content="متن مقاله هوش مصنوعی"),
-                Post(title="مقاله الگوریتم", content="متن مقاله الگوریتم")
-            ]
-            for post in sample_post:
-                db.session.add(post)
-            db.session.commit()
+        #9 delete this
+        # if Post.query.count() == 0:
+        #     sample_post = [
+        #         Post(title="مقاله لیزر", content="متن مقاله لیزر"),
+        #         Post(title="مقاله هوش مصنوعی", content="متن مقاله هوش مصنوعی"),
+        #         Post(title="مقاله الگوریتم", content="متن مقاله الگوریتم")
+        #     ]
+            # for post in sample_post:
+            #     db.session.add(post)
+            # db.session.commit()
 
 if __name__ == "__main__":
     create_tables()
